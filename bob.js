76000a371a7abe607e5cc2f80cca47da1e25ff5a
sanya2008 <74243016+sanya2008@users.noreply.github.com>
2021-02-23 15:37:40 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,30 @@
+class bob{
+    constructor(x,y)
+{
+    var options={
+        isStatic:false,
+        restitution:1,
+        friction:0,
+        density:5.8,
+    }
+
+    
+    this.body=Bodies.circle(x,y,25,options)
+    
+    World.add(world,this.body)
+
+}
+display()
+{
+    
+  
+   push ()
+   
+   ellipseMode(RADIUS)
+   fill ("purple")
+   ellipse(this.body.position.x,this.body.position.y,25,25)
+   pop ()
+
+
+}
+}
